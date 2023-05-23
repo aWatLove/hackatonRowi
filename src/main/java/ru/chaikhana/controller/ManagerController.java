1cad3fe1295b74d0334e47c7f87aeacc5b94f648
awatlove <watislove4u@gmail.com>
2023-05-23 12:00:43 +0400

    manager controller

---
+++ src/main/java/ru/chaikhana/controller/ManagerController.java
@@ -0,0 +1,21 @@
+package ru.chaikhana.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/manager")
+public class ManagerController {
+
+    @GetMapping("/getAllChat")
+    public ResponseEntity<?> getAllOpenChat(){
+        return ResponseEntity.ok("чаты лол");
+    }
+
+    @GetMapping("/openChat")
+    public ResponseEntity<?> openChat(String id){
+        return ResponseEntity.ok("chat");
+    }
+}
