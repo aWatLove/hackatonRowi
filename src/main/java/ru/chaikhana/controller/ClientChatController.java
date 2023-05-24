53dbb6751929aba0be02cd0d21a3891f760dedd9
Pavel <pavellgindemit@mail.ru>
2023-05-24 22:23:04 +0400

    No WS

--- src/main/java/ru/chaikhana/controller/ClientChatController.java
+++ src/main/java/ru/chaikhana/controller/ClientChatController.java
@@ -1,19 +1,12 @@
 package ru.chaikhana.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import ru.chaikhana.model.Message;
 import ru.chaikhana.model.chat.ClientChat;
-import ru.chaikhana.service.clientchat.ClientChatService;
 import ru.chaikhana.service.clientchat.ClientChatServiceImpl;
 import ru.chaikhana.service.message.MessageServiceImpl;
 
@@ -22,7 +15,6 @@ import java.util.List;
 @AllArgsConstructor
 @RestController
 public class ClientChatController {
-    private final SimpMessagingTemplate simpMessagingTemplate;
     private final ClientChatServiceImpl clientChatService;
     private final MessageServiceImpl messageService;
 
@@ -54,18 +46,5 @@ public class ClientChatController {
     }
 
 //===========================================================
-    @MessageMapping("/chat/sendMessage")
-    @SendTo("/topic/chat")
-    public Message sendMessage(@Payload Message message) {
-        return messageService.addMessage(message);
-    }
 
-    @MessageMapping("/chat.addUser")
-    @SendTo("/topic/public")
-    public Message addUser(@Payload Message message,
-                              SimpMessageHeaderAccessor headerAccessor) {
-        // Add username in web socket session
-        headerAccessor.getSessionAttributes().put("username", message.getSenderId());
-        return message;
-    }
 }
--- src/main/java/ru/chaikhana/controller/ManagerController.java
+++ src/main/java/ru/chaikhana/controller/ManagerController.java
@@ -1,7 +1,6 @@
 package ru.chaikhana.controller;
 
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
